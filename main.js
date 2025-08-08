56c3202b1979eb3b31de530c6daaf30d475254f4
greenALIAS48 <eric.amen.350@gmail.com>
2025-08-08 10:57:03 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-
-document.getElementById('root').innerHTML = `
-  <h1>FreightPro Frontend is Live</h1>
-  <p>Connected to backend via: ${import.meta.env.VITE_API_URL}</p>
-`;
